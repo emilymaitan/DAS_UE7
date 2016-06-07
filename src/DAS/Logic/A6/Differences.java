ed4a97da3cfd417278599b4b9c31e50f4171c8ad
Mai <a1306497@unet.univie.ac.at>
2016-06-07 22:27:46 +0200

    add logic for A6
    it's just one method, the abs part is temporary - until Boxplot works for
    negative values

---
+++ src/DAS/Logic/A6/Differences.java
@@ -0,0 +1,18 @@
+package DAS.Logic.A6;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Emily on 6/7/2016.
+ */
+public class Differences {
+    public static ArrayList<Integer> calcDifferences(ArrayList<Integer> set1, ArrayList<Integer> set2) {
+        if (set1.size() != set2.size()) return null;
+
+        ArrayList<Integer> res = new ArrayList<>(set1.size());
+        for (int i = 0; i < set1.size(); i++) {
+            res.add(i,Math.abs(set1.get(i)-set2.get(i)));
+        }
+        return res;
+    }
+}
